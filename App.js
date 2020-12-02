7a2a50870be45e050a64a5602990b64a278f3903
Martins Kreslins <martins.kreslins@va.lv>
2020-12-02 18:24:05 +0200

    updates App

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {useState} from 'react'
+import {createStackNavigator} from '@react-navigation/stack';
+import { View, Button, Text, StyleSheet} from 'react-native';
+import { FlatList} from 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function Home({navigation}){
+  const [counter, setCounter] = useState(0);
+  return(
+    
+    <View style = {styles.container}>
+     <Button
+    title="Go to List"
+    onPress={() => navigation.navigate('List')}
+    />
+     <Button
+    title="Increase counter value!"
+    onPress={() => {
+     setCounter(counter + 1) 
+    }}
+    />
+     <Button
+    title="Decrease counter value!"
+    onPress={() => {setCounter(counter - 1) 
+    }}
+    />
+    <Text style={styles.counter}>Counter value is {counter}!</Text>
     </View>
   );
 }
+function List(){
+const data =  new Array(10).fill(null).map((v, i) => ({ key: (i + 1).toString(), value: `John Doe` }));
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.headline}>John Does</Text>
+      <FlatList
+      data = {data}
+      renderItem={({item}) => 
+  <Text style = {styles.item}>{item.value} {item.key}</Text>
+    
+    }
+      />
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+
+export default function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options= {{title: 'Home'}}
+                   
+        />
+        <Stack.Screen name="List"
+        component={List}
+        options= {{title: 'List'}} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+      alignItems: 'stretch',
+      justifyContent: 'flex-start'
   },
+  item:{
+    textAlign: 'center',
+    fontSize:20
+
+  },
+  counter: {
+    textAlign: 'center',
+    fontStyle : 'italic',
+    fontSize : 25
+  },
+  headline: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 25,
+    marginTop: 0
+  }
+ 
 });
